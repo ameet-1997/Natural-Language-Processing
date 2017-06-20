bec896356311e292b5e34798b87bc365abe985f1
Ameet Deshpande <ameetsd97@gmail.com>
2017-06-20 15:50:27 +0530

    Add Docstring

--- Multi-Label/RCV1/generate_label_vs_number.py
+++ Multi-Label/RCV1/generate_label_vs_number.py
@@ -1,9 +1,13 @@
+"""
+Creates a list which stores the number od documents against the labels
+"""
+
 from sklearn.datasets import fetch_rcv1
 
 training_dataset = fetch_rcv1(subset='train')
 counts = training_dataset.target.todense()
 
-number_of_documents = []
+numbers_of_documents = []
 
 for i in range(len(training_dataset.target_names)):
 	number_of_documents.append(counts[:,i].flatten().sum())
