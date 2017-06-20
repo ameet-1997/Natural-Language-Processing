29f75c371858aa90928f57f9b97aee635ea82bb3
Ameet Deshpande <ameetsd97@gmail.com>
2017-06-20 15:49:10 +0530

    Update Variable Names

--- Multi-Label/RCV1/script_for_graph.py
+++ Multi-Label/RCV1/script_for_graph.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
- 
-objects = labels
+
 y_pos = np.arange(len(objects))
-performance = number_of_documents
- 
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
+# numbers_of_documents is a list which contains the number of documents per categories
+# labels are the label names that can be obtained from RCV1 dataset
+plt.bar(y_pos, numbers_of_documents, align='center', alpha=0.5)
+plt.xticks(y_pos, labels)
 plt.ylabel('Number of documents')
 plt.title('Categories in RCV1 Dataset')
  
